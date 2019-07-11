144abc4007576f5f78928d24022f74789813e164
Jonathan Azali <azali.jonathan@gmail.com>
2019-07-11 09:44:59 -0700

    after presentation comments

--- index.js
+++ index.js
@@ -1,81 +1,87 @@
-function card(suit, weight) {
-  this.suit = suit || '';
-  this.weight = weight || 0;
+function Card(suit = '', weight = 0) {
+  this.suit = suit;
+  this.weight = weight;
+  // TODO: Function to getScore()
 }
 
-let myDeck = {
-  cards: [],
+const myDeck = {
+  // TODO: Populate deck within class
+  cards: []
 };
 
-let player = {
-  currentCards: [],
+// TODO: Player and dealer should be classes
+const player = {
+  currentCards: []
 };
 
-let banker = {
-  currentCards: [],
+const dealer = {
+  currentCards: []
 };
 
 function populateDeck(deck) {
   // Create cards for each suit
-  let suits = ['♠', '♣', '♥', '♦'];
+  const suits = ['♠', '♣', '♥', '♦'];
   suits.forEach(suit => {
     for (i = 2; i <= 11; i++) {
-      let myCard = new card(suit, i);
+      const myCard = new Card(suit, i);
       if (i === 10) {
         myDeck.cards.push(myCard);
         myDeck.cards.push(myCard);
         myDeck.cards.push(myCard);
       }
+      // Insert cards into deck
       myDeck.cards.push(myCard);
     }
   });
-  // Insert cards into deck
 }
 
-function dealCards(player, banker) {
-  let tempNum = [];
+function dealCards(player, dealer) {
+  const tempNum = [];
   // get the random number
   // check if exists
   // push into array
   for (i = 0; i < 4; i++) {
-    let rand = Math.floor(Math.random() * 52);
+    const rand = Math.floor(Math.random() * 52);
     if (tempNum.includes(rand)) {
+      // If number repeats go through loop again
       i--;
     } else {
       tempNum.push(rand);
     }
   }
   player.currentCards.push(myDeck.cards[tempNum[0]]);
-  banker.currentCards.push(myDeck.cards[tempNum[1]]);
+  dealer.currentCards.push(myDeck.cards[tempNum[1]]);
   player.currentCards.push(myDeck.cards[tempNum[2]]);
-  banker.currentCards.push(myDeck.cards[tempNum[3]]);
+  dealer.currentCards.push(myDeck.cards[tempNum[3]]);
 }
 
-function checkWin(player, banker) {
+function checkWin(player, dealer) {
   let playerScore = 0;
-  let bankerScore = 0;
+  let dealerScore = 0;
+  // TODO: Add bust if there is two aces
+  // Will be replaced with reduce function
   player.currentCards.forEach(card => {
     playerScore += card.weight;
   });
-  banker.currentCards.forEach(card => {
-    bankerScore += card.weight;
+  dealer.currentCards.forEach(card => {
+    dealerScore += card.weight;
   });
-  if (playerScore > bankerScore) {
+  if (playerScore > dealerScore) {
     console.log('Player Wins!');
-  } else if (playerScore < bankerScore) {
-    console.log('Banker Wins!');
+  } else if (playerScore < dealerScore) {
+    console.log('dealer Wins!');
   } else console.log("It's a tie!");
 
   console.log(`Player Score: ${playerScore}`);
-  console.log(`Banker Score: ${bankerScore}`);
+  console.log(`dealer Score: ${dealerScore}`);
 }
 
-function printGame(player, banker) {
+function printGame(player, dealer) {
   console.log('======================');
   console.log('  Welcome to 二十一点');
   console.log('======================\n');
   console.log(`Dealer's Hand:`);
-  banker.currentCards.forEach(card => {
+  dealer.currentCards.forEach(card => {
     console.log(`{Suit: ${card.suit}, Weight: ${card.weight}}`);
   });
   console.log(`\nPlayer's Hand:`);
@@ -87,9 +93,9 @@ function printGame(player, banker) {
 
 function playGame() {
   populateDeck(myDeck);
-  dealCards(player, banker);
-  printGame(player, banker);
-  checkWin(player, banker);
+  dealCards(player, dealer);
+  printGame(player, dealer);
+  checkWin(player, dealer);
 }
 
 playGame();
